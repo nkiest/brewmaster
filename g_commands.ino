295218001cd6125c4721cb956fb03482d5794470
Nick Kiest <simnick@mac.com>
2014-08-01 14:00:37 -0700

    Check valid commands in array
    Made lists of valid commands to check against, so that subroutines
    should only get valid data.

--- g_commands.ino
+++ g_commands.ino
@@ -1,26 +1,80 @@
-void commands(){
-  if (currentCommand == "whirlpoolValve"){
-    if ((commandArg1 == "on" )|| (commandArg1 == "off")){
-      if (commandArg1 == "on" ){
-        valve(whirlpoolValvePin, 1);
-      }
-      if (commandArg1 == "off" ){
-        valve(whirlpoolValvePin, 0);
-      }
-    }
-    else {
-      Serial << commandNumber << ": Error: Invalid argument" << endl;
-      clearCommand();
-      return;
+String validCommands[10] = {"grind","fill","heatTo","boil","wortPump","whirlpoolValve","drainValve","wortPipeValve","CIPPump","holdAt"};
+String zeroArgCommands[] = {"grind"};
+String singleNumericArgCommands[] = {"fill","heatTo","boil"};
+String boolCommands[] = {"wortPump","whirlpoolValve","drainValve","wortPipeValve","CIPPump"};
+String doubleNumericCommands[] = {"holdAt"};
+String highLevelCommands[] = {"recirculate","whirlpool","drain","liftMash","dumpMash","CIP","lid"}; //not implemented yet
+
+//checks for valid command number, runs commands();
+void commandHandler(){
+  commandNumber = commandPart[0].toInt();
+  if (commandNumber == 0){
+    Serial << "Error: Invalid command number" << endl;
+    clearCommand();
+    return;
+  }
+  currentCommand = commandPart[1];
+  
+  if (currentCommand == ""){
+    Serial << commandNumber << ": Error: No command given" << endl;
+    clearCommand();
+    return;
+  }
+  commandArg1 = commandPart[2];
+  commandArg2 = commandPart[3];
+  
+  int j;
+  bool valid;
+  for (j = 0; j < 10; j += 1){
+    if (validCommands[j] == currentCommand){
+      valid = true;
     }
   }
+  if (valid == false){
+  Serial << commandNumber << ": Error: Invalid Command" << endl;
+    clearCommand();
+    return;
+  }
+  
+  int PIN;
+  bool on;
+
+  //on off commands
+  boolCommand();
 }
 
-//const int whirlpoolValvePin = 24;
-//const int wortPipeValvePin = 25;
-//const int drainValvePin = 26;
-
-void valve(int PIN, bool on){
+void boolCommand(){
+  int PIN;
+  bool on;
+  if (currentCommand == "whirlpoolValve") {
+    PIN = whirlpoolValvePin;
+  }
+  else if (currentCommand == "wortPipeValve") {
+    PIN = wortPipeValvePin;
+  }
+  else if (currentCommand == "drainValve") {
+    PIN = drainValvePin;
+  }
+  else if (currentCommand == "wortPump") {
+    PIN = wortPumpPin;
+  }
+  else if (currentCommand == "CIPPump") {
+    PIN = CIPPumpPin;
+  }
+  else {
+    return;
+  }
+  if (commandArg1 == "on") {
+    on = true;
+  }
+  else if (commandArg1 == "off") {
+    on = false;
+  }
+  else {
+    Serial << commandNumber << ": Error: Invalid argument" << endl;
+    clearCommand();
+    return;
+  }
   if ((on == true) && (digitalRead(PIN) == HIGH)){
     Serial << commandNumber << ": Error: " << currentCommand << ": " << commandArg1 << " already ON" << endl;
     clearCommand();
@@ -37,28 +91,14 @@ void valve(int PIN, bool on){
   else if (on == false){
     digitalWrite(PIN, LOW);
   }
-  commandDelay = 2200;
+ commandDelay = 2200;
   Serial << commandNumber << ": " << currentCommand << ": " << commandArg1 << " received" << endl;
 
 }
 
-void commandHandler(){
-  commandNumber = commandPart[0].toInt();
-  if (commandNumber == 0){
-    Serial << "Error: Invalid command number" << endl;
-    clearCommand();
-    return;
-  }
-  currentCommand = commandPart[1];
-  if (currentCommand == ""){
-    Serial << commandNumber << ": Error: No command given" << endl;
-    clearCommand();
-    return;
-  }
-  commandArg1 = commandPart[2];
-  commandArg2 = commandPart[3];
-  commands();
-}
+
+ 
+
 void clearCommand(){
   commandPart[0] = "";
   commandNumber = 0;
@@ -94,4 +134,5 @@ void switchWortPump(boolean command, int commandCode){
     }
   }
 }
+
 
